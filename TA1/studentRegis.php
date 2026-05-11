c0f7a666702f95cdb0124282acf6836077bc157e
Chipsyyyy <202410116@fit.edu.ph>
2026-05-11 18:11:40 +0800

    css

--- TA1/studentRegis.php
+++ TA1/studentRegis.php
@@ -2,3 +2,69 @@
 <html>
 <head>
     <title>Student Registration Form</title>
+    <style>
+        body {
+            font-family: Arial;
+            font-size: 13px;
+        }
+
+        .container {
+            width: 800px;
+            margin: auto;
+            border: 1px solid black;
+            padding: 20px;
+        }
+
+        h2 {
+            text-align: right;
+        }
+
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-bottom: 8px;
+        }
+
+        td {
+            border: 1px solid black;
+            padding: 6px;
+        }
+
+        .title {
+            background-color: black;
+            color: white;
+            font-weight: bold;
+            padding: 5px;
+        }
+
+        .line {
+            border-bottom: 1px solid black;
+            display: inline-block;
+            width: 150px;
+            text-align: center;
+        }
+
+        .long-line {
+            border-bottom: 1px solid black;
+            display: inline-block;
+            width: 400px;
+            text-align: center;
+        }
+
+        .box {
+            display: inline-block;
+            width: 10px;
+            height: 10px;
+            border: 1px solid black;
+        }
+
+        .checked {
+            background-color: black;
+        }
+
+        .small-text {
+            font-size: 10px;
+            text-align: center;
+        }
+    </style>
+</head>
