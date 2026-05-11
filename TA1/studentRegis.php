490ff24e06ab78172356e99a08317f9def1dc2f4
Chipsyyyy <202410116@fit.edu.ph>
2026-05-11 18:37:49 +0800

    variables

--- TA1/studentRegis.php
+++ TA1/studentRegis.php
@@ -69,3 +69,57 @@
         }
     </style>
 </head>
+
+<body>
+
+<?php
+$studentNumber = "202410116";
+$date = "May 11, 2026";
+$entryDate = "August 12, 2026";
+$entryType = "New Student";
+$yearLevel = "3RD Year";
+$section = "BSIT TW22";
+
+$collegeDepartment = "College of Computer Studies";
+$program = "BS Information Technology";
+$major = "Web and Mobile Application Development";
+$semester = "1st Semester";
+$schoolYear = "2026-2027";
+
+$lastName = "Nieves";
+$firstName = "Sam Kenneth";
+$middleName = "Peralta";
+$nickname = "Riki";
+$gender = "Male";
+$birthYear = "2006";
+$birthMonth = "January";
+$birthDay = "12";
+$placeOfBirth = "Manila";
+$civilStatus = "Single";
+
+// Previous school variables
+$previousSchool = "Balungao National High School";
+$lastDateAttended = "March 30, 2026";
+$strandCourse = "ICT Strand";
+$schoolAddress = "Poblacion, Balungao, Pangasinan";
+$reasonTransfer = "To continue college studies";
+
+// Health variables
+$medicalConditions = "N/A";
+$emergencyContactName = "Francine Kate N. Riel";
+$emergencyContactNumber = "09123456789";
+
+// Citizenship variables
+$birthCountry = "Philippines";
+$provinceOfBirth = "Tayug, Pangasinan";
+$countryCitizenship = "Philippines";
+$religion = "Roman Catholic";
+
+// String formatting
+$lastName = strtoupper($lastName);
+$firstName = ucfirst($firstName);
+$middleName = ucfirst($middleName);
+$nickname = ucfirst($nickname);
+$previousSchool = strtoupper($previousSchool);
+$program = strtoupper($program);
+?>
